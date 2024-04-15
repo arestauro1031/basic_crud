f24db0fea76522cd5ba1500e8620883b5fd42835
Andrian <132529758+ABRestauro@users.noreply.github.com>
2024-04-15 10:17:29 +0800

    first commit

--- employee_register/models.py
+++ employee_register/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Position(models.Model):
+    
+    positionID = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    
+    empID = models.IntegerField(primary_key=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    
+    mobileno = models.CharField(max_length=50)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    
